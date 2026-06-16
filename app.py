419219871d1231c7d50866fb8341c5e7aa0e2c66
Hari Ram <ramh772004@gmail.com>
2026-06-16 19:20:02 +0530

    Updated model with 3 features

--- app.py
+++ app.py
@@ -2,21 +2,27 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load("naive_bayes_model.pkl")
+model = joblib.load("xgboost_model.pkl")
 
-x1 = st.number_input("Tool Diversity")
-x2 = st.number_input("Weekly GenAI Hours")
-x3 = st.number_input("Traditional Study Hours")
+st.title("Student Performance Predictor")
+
+tool_diversity = st.number_input("Tool Diversity")
+weekly_genai_hours = st.number_input("Weekly GenAI Hours")
+traditional_study_hours = st.number_input("Traditional Study Hours")
 
 if st.button("Predict"):
+
     data = pd.DataFrame(
-        [[x1, x2, x3]],
+        [[tool_diversity,
+          weekly_genai_hours,
+          traditional_study_hours]],
         columns=[
-            'Tool_Diversity',
-            'Weekly_GenAI_Hours',
-            'Traditional_Study_Hours'
+            "Tool_Diversity",
+            "Weekly_GenAI_Hours",
+            "Traditional_Study_Hours"
         ]
     )
 
     prediction = model.predict(data)
-    st.success(f"Predicted GPA: {prediction[0]:.2f}")
+
+    st.success(f"Prediction: {prediction[0]}")
